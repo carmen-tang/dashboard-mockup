ff9bc02f7645e6f97ddd6607e9a741a4d8831269
Carmen Tang <carmen.tang1@gmail.com>
2024-12-30 14:57:17 -0800

    top nav edits - styles

--- app/dashboard/topNav.tsx
+++ app/dashboard/topNav.tsx
@@ -1,9 +1,10 @@
 const TopNav: React.FC<{ topNavLeft: string[]; topNavRight: string[] }> = ({ topNavLeft, topNavRight }) => (
-  <nav className="top-nav sticky inset-0 box-border inline-flex flex-grow w-full h-auto py-1 overflow-hidden">
+  <nav className="top-nav inset-0 box-border inline-flex flex-grow w-full h-auto p-6 bg-[#f1f1f3]">
     <div className="top-nav-left flex flex-grow items-start">
       Coverage Periods
     </div>
     <div className="top-nav-right">
+      <span className="bg-black p-3 rounded-lg text-white mr-2">HK</span>
       Hanna
     </div>
   </nav>
