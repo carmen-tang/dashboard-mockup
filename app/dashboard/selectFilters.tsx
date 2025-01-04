131de6116e4fae1516d05a50af247926674d1302
Carmen Tang <carmen.tang1@gmail.com>
2025-01-04 14:04:41 -0800

    select filters menu - add coverage end/setup complete fields

--- app/dashboard/selectFilters.tsx
+++ app/dashboard/selectFilters.tsx
@@ -11,7 +11,7 @@ const SelectFilters: React.FC = () => {
   const [activeTab, setActiveTab] = useState<Tab>('Filters');
   
   return (
-    <div className="select-filters z-10 bg-white shadow-xl h-full w-[400px] absolute right-0 top-0">
+    <div className="select-filters z-10 bg-white shadow-xl h-full w-[450px] absolute right-0 top-0">
       <h2 className="flex justify-between text-2xl mb-2 p-6">Select Filters <IoCloseOutline className="large-icon" /></h2>
 
       <div className="tabs border-b border-gray-200 pl-6">
@@ -31,6 +31,7 @@ const SelectFilters: React.FC = () => {
             <label>Organization Name</label>
             <select className=""></select>
 
+            {/* group */}
             <div className="relative my-2">
               <label htmlFor="group" className="font-semibold">Group</label>
               <select id="group" className="select-dropdown border rounded-lg border-gray-300 w-full p-2">
@@ -42,6 +43,7 @@ const SelectFilters: React.FC = () => {
               <FaChevronDown className="absolute right-4 top-9" />
             </div>
 
+            {/* coverage start date */}
             <div className="flex flex-col my-4">
               <label htmlFor="coverage-start-date" className="font-semibold">Coverage Start Date</label>
               <div className="flex relative">
@@ -57,12 +59,12 @@ const SelectFilters: React.FC = () => {
                 <div className="flex-grow w-3/6 ml-1">
                   <DatePicker className="select-dropdown border rounded-lg border-gray-300 w-full p-2">
                     <Group className="flex justify-between">
-                      <DateInput className="flex">
+                      <DateInput className="flex uppercase">
                         {(segment) => <DateSegment segment={segment} />}
                       </DateInput>
                       <Button><LuCalendar1 className="small-icon" /></Button>
                     </Group>
-                    <Popover>
+                    <Popover className="bg-white shadow-md rounded-xl p-2 mt-2">
                       <Dialog>
                         <Calendar>
                           <div className="flex justify-between mt-4">
@@ -71,7 +73,7 @@ const SelectFilters: React.FC = () => {
                             <Button slot="next">▶</Button>
                           </div>
                           <CalendarGrid>
-                            {(date) => <CalendarCell date={date} />}
+                            {(date) => <CalendarCell date={date} className="text-center" />}
                           </CalendarGrid>
                         </Calendar>
                       </Dialog>
@@ -81,6 +83,85 @@ const SelectFilters: React.FC = () => {
               </div>
             </div>
 
+            {/* coverage end date */}
+            <div className="flex flex-col my-4">
+              <label htmlFor="coverage-start-date" className="font-semibold">Coverage End Date</label>
+              <div className="flex relative">
+                <div className="flex-grow w-3/6 relative mr-1">
+                  <select id="group" className="select-dropdown border rounded-lg border-gray-300 w-full p-2">
+                    <option value=""></option>
+                    <option value="group1">Group 1</option>
+                    <option value="group2">Group 2</option>
+                    <option value="group3">Group 3</option>
+                  </select>
+                  <FaChevronDown className="absolute right-4 top-3" />
+                </div>
+                <div className="flex-grow w-3/6 ml-1">
+                  <DatePicker className="select-dropdown border rounded-lg border-gray-300 w-full p-2">
+                    <Group className="flex justify-between">
+                      <DateInput className="flex uppercase">
+                        {(segment) => <DateSegment segment={segment} />}
+                      </DateInput>
+                      <Button><LuCalendar1 className="small-icon" /></Button>
+                    </Group>
+                    <Popover className="bg-white shadow-md rounded-xl p-2 mt-2">
+                      <Dialog>
+                        <Calendar>
+                          <div className="flex justify-between mt-4">
+                            <Button slot="previous">◀</Button>
+                            <Heading />
+                            <Button slot="next">▶</Button>
+                          </div>
+                          <CalendarGrid>
+                            {(date) => <CalendarCell date={date} className="text-center" />}
+                          </CalendarGrid>
+                        </Calendar>
+                      </Dialog>
+                    </Popover>
+                  </DatePicker>
+                </div>
+              </div>
+            </div>
+
+            {/* setup complete at */}
+            <div className="flex flex-col my-4">
+              <label htmlFor="coverage-start-date" className="font-semibold">Setup Complete At</label>
+              <div className="flex relative">
+                <div className="flex-grow w-3/6 relative mr-1">
+                  <select id="group" className="select-dropdown border rounded-lg border-gray-300 w-full p-2">
+                    <option value=""></option>
+                    <option value="group1">Group 1</option>
+                    <option value="group2">Group 2</option>
+                    <option value="group3">Group 3</option>
+                  </select>
+                  <FaChevronDown className="absolute right-4 top-3" />
+                </div>
+                <div className="flex-grow w-3/6 ml-1">
+                  <DatePicker className="select-dropdown border rounded-lg border-gray-300 w-full p-2">
+                    <Group className="flex justify-between">
+                      <DateInput className="flex uppercase">
+                        {(segment) => <DateSegment segment={segment} />}
+                      </DateInput>
+                      <Button><LuCalendar1 className="small-icon" /></Button>
+                    </Group>
+                    <Popover className="bg-white shadow-md rounded-xl p-2 mt-2">
+                      <Dialog>
+                        <Calendar>
+                          <div className="flex justify-between mt-4">
+                            <Button slot="previous">◀</Button>
+                            <Heading />
+                            <Button slot="next">▶</Button>
+                          </div>
+                          <CalendarGrid>
+                            {(date) => <CalendarCell date={date} className="text-center" />}
+                          </CalendarGrid>
+                        </Calendar>
+                      </Dialog>
+                    </Popover>
+                  </DatePicker>
+                </div>
+              </div>
+            </div>
 
           </div>
         ) : (
